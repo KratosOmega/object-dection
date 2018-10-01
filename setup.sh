7d8557c596a1f4882c40eaafbe32e038fa93c9a4
Kratos <Kratos.X.Omega@gmail.com>
2018-09-30 21:10:14 -0400

    initial

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------
+pip3 install pandas
+# ---------------------------------------------------------
+python3 ./models/research/setup.py build
+python3 ./models/research/setup.py install
+
