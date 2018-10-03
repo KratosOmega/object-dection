6418c3e71acae39852d5dce03181958ee63392f6
xin <li4x7@mail.uc.edu>
2018-10-03 01:25:07 -0400

    reset

--- setup.sh
+++
@@ -1,6 +0,0 @@
-# ---------------------------------------------------------
-pip3 install pandas
-# ---------------------------------------------------------
-python3 ./models/research/setup.py build
-python3 ./models/research/setup.py install
-
